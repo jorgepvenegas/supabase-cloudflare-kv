5a8867bbf07435fd0cd00ca56aa66f4c86cec2c5
Jorge Venegas <jorgepvenegas@protonmail.com>
2022-09-20 23:18:26 -0700

    formatting

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import { createClient } from '@supabase/supabase-js';
-import { Router } from 'itty-router';
-import { json, status, withContent } from 'itty-router-extras';
-import { readFrom, writeTo } from './utils/cache';
+import { createClient } from "@supabase/supabase-js";
+import { Router } from "itty-router";
+import { json, status, withContent } from "itty-router-extras";
+import { readFrom, writeTo } from "./utils/cache";
 
 const router = new Router();
 
@@ -15,77 +15,86 @@ const router = new Router();
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-router.get('/articles', async (request, {SUPABASE_URL, SUPABASE_ANON_KEY, ARTICLES}) => {
-	const cachedArticles = await readFrom(ARTICLES, '/articles');
-	if(cachedArticles) {
-		// console.log('returning cached articles');
-		return json(cachedArticles);
-	}
+router.get(
+  "/articles",
+  async (request, { SUPABASE_URL, SUPABASE_ANON_KEY, ARTICLES }) => {
+    const cachedArticles = await readFrom(ARTICLES, "/articles");
+    if (cachedArticles) {
+      // console.log('returning cached articles');
+      return json(cachedArticles);
+    }
 
-	// console.log('returning fresh articles');
-	const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-	const { data } = await supabase.from('articles').select('*');
-	await writeTo(ARTICLES, '/articles', data);
-	return json(data);
+    // console.log('returning fresh articles');
+    const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+    const { data } = await supabase.from("articles").select("*");
+    await writeTo(ARTICLES, "/articles", data);
+    return json(data);
+  }
+);
+
+router.get(
+  "/articles/:id",
+  async (request, { SUPABASE_URL, SUPABASE_ANON_KEY, ARTICLES }) => {
+    const { id } = request.params;
+    const cachedArticle = await readFrom(ARTICLES, `/articles/${id}`);
+    if (cachedArticle) {
+      // console.log('returning cached article');
+      return json(cachedArticle);
+    }
+
+    // console.log('returning fresh article');
+    const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+    const { data } = await supabase
+      .from("articles")
+      .select("*")
+      .filter("id", "eq", request.params.id);
+    if (!data) {
+      return status(404, "Not found");
+    }
+    await writeTo(ARTICLES, `/articles/${id}`, data);
+    return json(data);
+  }
+);
+
+router.get("/write-kv", async (request, { ARTICLES }) => {
+  const articles = [{ title: "Test 1" }, { title: "Teeeeeest" }];
+
+  await writeTo(ARTICLES, "/articles", articles);
+  return json(articles);
 });
 
-router.get('/articles/:id', async (request, {SUPABASE_URL, SUPABASE_ANON_KEY, ARTICLES}) => {
-	const { id } = request.params;
-	const cachedArticle = await readFrom(ARTICLES, `/articles/${id}`);
-	if(cachedArticle) {
-		// console.log('returning cached article');
-		return json(cachedArticle);
-	}
-
-	// console.log('returning fresh article');
-	const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-	const { data } = await supabase.from('articles').select('*').filter('id', 'eq', request.params.id);
-	if(!data) {
-		return status(404, "Not found")
-	}
-	await writeTo(ARTICLES, `/articles/${id}`, data);
-	return json(data);
+router.get("/read-kv", async (request, { ARTICLES }) => {
+  const articles = await readFrom(ARTICLES, "/articles");
+  return json(articles);
 });
-
-router.get('/write-kv', async(request, { ARTICLES }) => {
-	const articles = [{title: 'Test 1'}, {title: 'Teeeeeest'}]
-
-	await writeTo(ARTICLES, '/articles', articles);
-	return json(articles);
-})
-
-router.get('/read-kv', async(request, { ARTICLES }) => {
-	const articles = await readFrom(ARTICLES, '/articles');
-	return json(articles);
-})
 
 router.post(
-	'/revalidate', 
-	withContent,
-	async(request, {SUPABASE_URL, SUPABASE_ANON_KEY, ARTICLES}, context) => {
-		const updateCache = async () => {
-			const { type, record, old_record } = request.content;
-			const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-			if(type === 'INSERT' || type === 'UPDATE') {
-				await writeTo(ARTICLES, `/articles/${record.id}`, record);	
-			}
-	
-			if(type === 'DELETE') {
-				await ARTICLES.delete(`/articles/${old_record.id}`)
-			}
-				
-			const { data: articles } = await supabase.from('articles').select('*');
-			await writeTo(ARTICLES, '/articles', articles);
-		}
+  "/revalidate",
+  withContent,
+  async (request, { SUPABASE_URL, SUPABASE_ANON_KEY, ARTICLES }, context) => {
+    const updateCache = async () => {
+      const { type, record, old_record } = request.content;
+      const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+      if (type === "INSERT" || type === "UPDATE") {
+        await writeTo(ARTICLES, `/articles/${record.id}`, record);
+      }
 
-		context.waitUntil(updateCache());
+      if (type === "DELETE") {
+        await ARTICLES.delete(`/articles/${old_record.id}`);
+      }
 
-		return json({received: true});
-	}
-)
+      const { data: articles } = await supabase.from("articles").select("*");
+      await writeTo(ARTICLES, "/articles", articles);
+    };
 
-router.all('*', () => status(404, 'Not found'))	
+    context.waitUntil(updateCache());
+
+    return json({ received: true });
+  }
+);
+
+router.all("*", () => status(404, "Not found"));
 
 export default {
-	fetch: router.handle
+  fetch: router.handle,
 };
